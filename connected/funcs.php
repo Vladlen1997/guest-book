5f38535ca102d6e6f9ec7970343ea4a5283d80c3
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2023-05-11 12:24:37 +0300

    checked the login and password and displayed the corresponding message

--- connected/funcs.php
+++ connected/funcs.php
@@ -2,5 +2,21 @@
 
 function debug($data)
 {
-    echo '<pre>' . print_r($data) . '</pre>';
+    echo '<pre>' . print_r($data, 1) . '</pre>';
+}
+
+
+function registration(): bool
+{
+    global $pdo;
+
+    $login = !empty($_POST['login']) ? trim($_POST['login']) : '';
+    $pass = !empty($_POST['pass']) ? trim($_POST['pass']) : '';
+
+    if (empty($login) || empty($pass)) {
+        $_SESSION['errors'] = 'Поля логин/пароль обязательны!';
+        return false;
+    }
+
+
 }
